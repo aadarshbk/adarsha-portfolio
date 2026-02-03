40487c51934689082a841ddd9b75e93425630891
aadarshbk <babiaadarsh@gmail.com>
2026-02-03 16:23:51 +0545

    Initial clean commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import AboutMe from './_components/AboutMe';
+import Banner from './_components/Banner';
+import Experiences from './_components/Experiences';
+import Skills from './_components/Skills';
+
+export default function Home() {
+    return (
+        <div className="page-">
+            <Banner />
+            <AboutMe />
+            <Skills />
+            <Experiences />
+        </div>
+    );
+}
