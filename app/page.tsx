62f7e85164d5f0d7ddc4da60a7f0d56be7e22c1d
aadarshbk <babliaadarsh@gmail.com>
2026-04-15 23:30:16 +0545

    feat : implemennted chatbot for appointment

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import AboutMe from './_components/AboutMe';
 import Banner from './_components/Banner';
 import Experiences from './_components/Experiences';
 import Skills from './_components/Skills';
+import ChatWidgetLauncher from '@/components/ChatWidget';
+
+const apiBaseUrl = process.env.API_BASE_URL;
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.API_SECRET;
 
 export default function Home() {
     return (
@@ -10,6 +15,14 @@ export default function Home() {
             <AboutMe />
             <Skills />
             <Experiences />
+            <ChatWidgetLauncher
+                apiBaseUrl={apiBaseUrl}
+                apiKey={apiKey}
+                apiSecret={apiSecret}
+                title="Ai Support Bot"
+                subtitle="Aadarsh Bot"
+                greeting="Hello!, How can I help you today?"
+            />
         </div>
     );
 }
